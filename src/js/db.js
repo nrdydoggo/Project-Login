f7b69391a138951f411618ce8f27f862d6b6c1e8
DoggoD <84948603+nrdydoggo@users.noreply.github.com>
2022-06-03 08:08:39 +0900

    Update db.js

--- src/js/db.js
+++ src/js/db.js
@@ -3,7 +3,7 @@ var userPW = "1234";
 var username = "Jordan";
 function userinfo()
 {
-document.write(" "+ username + ".<br>");
+document.write(" " + username + ".<br>");
 document.write("Your ID is '" + userID + "'.");
 document.write("<br>");
 document.write("Your Password is '" + userPW + "'.");
